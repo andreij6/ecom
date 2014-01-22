e790e7437eaa79f3d55c27079f2e93c49c5faa88
Andre Jones <andreij6@gmail.com>
2014-01-22 10:47:23 -0600

    Refactored create_spec tests

--- spec/features/products/create_spec.rb
+++ spec/features/products/create_spec.rb
@@ -2,16 +2,26 @@ require 'spec_helper'
 # rspec spec/features/products/create_spec.rb
 
 describe "Creating products" do
-  it "redirects to the products index page on success" do
+  
+  def create_product(options={})
+    options[:title] ||= "T-Shirt"
+    options[:price] ||= "T-Shirt"
+    options[:description] ||= "T-Shirt"
+    options[:image_url] ||= "T-Shirt"
+    
     visit "/products"
     click_link "New Product"
     expect(page).to have_content("New product")
     
-    fill_in "Title", with: "T-Shirt"
-    fill_in "Price", with: 2.99
-    fill_in "Description", with: "Summer Classic White T-shirt"
-    fill_in "Image url", with: "TShirt.png"
+    fill_in "Title", with: options[:title]
+    fill_in "Price", with: options[:price]
+    fill_in "Description", with: options[:description]
+    fill_in "Image url", with: options[:image_url]
     click_button "Create Product"
+  end
+  
+  it "redirects to the products index page on success" do
+    create_product
     
     expect(page).to have_content("T-Shirt")
   end
@@ -19,15 +29,7 @@ describe "Creating products" do
   it "displays an error when the product has no title" do
     expect(Product.count).to eq(0)
     
-    visit "/products"
-    click_link "New Product"
-    expect(page).to have_content("New product")
-    
-    fill_in "Title", with: ""
-    fill_in "Price", with: 2.99
-    fill_in "Description", with: "Summer Classic White T-shirt"
-    fill_in "Image url", with: "TShirt.png"
-    click_button "Create Product"
+    create_product title: ""
     
     expect(page).to have_content("error")
     expect(Product.count).to eq(0)
